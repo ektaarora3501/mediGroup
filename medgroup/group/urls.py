c7522c68acd88fd0dd8a68c3e68b473ea8aeaab3
Ekta <ektaarora3501@gmail.com>
2019-11-10 15:52:12 +0530

    Creating urls path

--- medgroup/group/urls.py
+++ medgroup/group/urls.py
@@ -2,5 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns=[
-     
+     path('',views.index,name='home'),
+     path('register',views.Signup,name='register'),
+     path('login',views.Login,name='login'),
+     path('userpage/<user>',views.dashboard,name='dashboard'),
 ]
