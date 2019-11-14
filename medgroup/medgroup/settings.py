6f4acc57c91aacab80f6283b295ac4bdce711cf6
Ekta <ektaarora3501@gmail.com>
2019-11-15 00:23:35 +0530

    Adding email settings

--- medgroup/medgroup/settings.py
+++ medgroup/medgroup/settings.py
@@ -27,6 +27,13 @@ DEBUG = True
 
 ALLOWED_HOSTS = []
 
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = '********************@gmail.com'
+EMAIL_HOST_PASSWORD = '+++++++++'
+EMAIL_PORT = 587
+
+
 
 # Application definition
 
@@ -38,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'group.apps.GroupConfig',
-]
+
+    ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
