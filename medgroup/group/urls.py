7e0e15db5d05a117917c64d73fb84d87aca5655b
Ekta <ektaarora3501@gmail.com>
2019-11-15 00:23:02 +0530

    Adding urls for different views

--- medgroup/group/urls.py
+++ medgroup/group/urls.py
@@ -4,7 +4,10 @@ from . import views
 urlpatterns=[
      path('',views.index,name='home'),
      path('register',views.Signup,name='register'),
+     path('send_mail/<phone>',views.Send,name='send_verifi_ph'),
+     path('verify_phone/<code>/<phone>',views.verify_ph,name='verify_phone'),
      path('login',views.Login,name='login'),
+     path('forgot_pass',views.forgot_pass,name='forgot_pass'),
      path('userpage/<user>',views.dashboard,name='dashboard'),
      path('logout/<user>',views.logout,name='logout'),
 ]
