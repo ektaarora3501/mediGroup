28868694cdf7df5993de3ce7382c302ad005e492
Ekta <ektaarora3501@gmail.com>
2019-11-16 00:37:10 +0530

    Adding basic settings for mail

--- medgroup/medgroup/settings.py
+++ medgroup/medgroup/settings.py
@@ -29,8 +29,8 @@ ALLOWED_HOSTS = []
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '********************@gmail.com'
-EMAIL_HOST_PASSWORD = '+++++++++'
+EMAIL_HOST_USER = '++++++++++++++++@gmail.com'
+EMAIL_HOST_PASSWORD = '**************'
 EMAIL_PORT = 587
 
 
