dde9529325897b921cd0b3b5adca781a9cd5720c
Ekta <ektaarora3501@gmail.com>
2019-11-16 22:25:48 +0530

    Adding redis server(backend for channel)

--- medgroup/medgroup/settings.py
+++ medgroup/medgroup/settings.py
@@ -15,7 +15,6 @@ import os
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
@@ -29,8 +28,8 @@ ALLOWED_HOSTS = []
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '++++++++++++++++@gmail.com'
-EMAIL_HOST_PASSWORD = '**************'
+EMAIL_HOST_USER = 'iamdeveloper3553@gmail.com'
+EMAIL_HOST_PASSWORD = 'creator3553'
 EMAIL_PORT = 587
 
 
@@ -38,6 +37,7 @@ EMAIL_PORT = 587
 # Application definition
 
 INSTALLED_APPS = [
+    'channels',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -77,10 +77,21 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'medgroup.wsgi.application'
-
+ASGI_APPLICATION = 'medgroup.routing.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
+
+CHANNEL_LAYERS = {
+    'default': {
+        'BACKEND': 'channels_redis.core.RedisChannelLayer',
+        'CONFIG': {
+            "hosts": [('127.0.0.1', 6379)],
+        },
+    },
+}
+
+
 
 DATABASES = {
      'default': {
