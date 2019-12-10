162956972e3afabc7019870352749d7c8abf6511
Ekta <ektaarora3501@gmail.com>
2019-12-10 22:38:47 +0530

    Basic settings

--- medgroup/medgroup/settings.py
+++ medgroup/medgroup/settings.py
@@ -28,7 +28,7 @@ ALLOWED_HOSTS = ['*']
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = '+****************@gmail.com'
+EMAIL_HOST_USER = '+++++++++++++++@gmail.com'
 EMAIL_HOST_PASSWORD = '*************'
 EMAIL_PORT = 587
 
