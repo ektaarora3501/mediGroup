d67bf5addb53a2746a5f55fecdce341f4e85bf20
Ekta Arora <42875737+ektaarora3501@users.noreply.github.com>
2020-02-17 19:05:37 +0530

    Update check_model.py

--- medgroup/check_model.py
+++ medgroup/check_model.py
@@ -4,14 +4,14 @@ from twilio.rest import Client
 
 # Your Account Sid and Auth Token from twilio.com/console
 # DANGER! This is insecure. See http://twil.io/secure
-account_sid = 'ACb4c5a96395c237ff38b4adc330425caa'
-auth_token = 'e07bb5ff29569b5c5022cbef56920007'
+account_sid = '*******************'
+auth_token = '++++++++++++++++++++++++++'
 client = Client(account_sid, auth_token)
 
 message = client.messages.create(
                               body='Hello there!',
                               from_='whatsapp:+14155238886',
-                              to='whatsapp:+918360581227'
+                              to='whatsapp:+++++++++++'
                           )
 
 print(message.sid)
